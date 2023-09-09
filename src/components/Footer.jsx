a133c2772ddc816ed152f8c79ae8086b5ae8c2c1
marksikaundi <emmanuelmark117@gmail.com>
2023-09-09 08:14:28 +0200

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,132 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+
+import React from "react";
+import Link from "next/link";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+          Beta-Web
+        </h1>
+        <p className="py-4">
+          DevCircle Africa is a vibrant community of technology enthusiasts and
+          professionals who are passionate about exploring the latest trends and
+          innovations in the tech industry. You can learn more about us by
+          visiting...{" "}
+          <a href="/about" className="font-semibold text-indigo-600">
+            <span className="absolute inset-0" aria-hidden="true" />
+            Read more <span aria-hidden="true">&rarr;</span>
+          </a>
+        </p>
+        <div className="flex justify-between md:w-[78%] my-6">
+          <Link href="https://facebook.com/Alisikaundi">
+            <FaFacebookSquare size={25} />{" "}
+          </Link>
+          <Link href="https://github.com/cs60apa">
+            <FaGithubSquare size={25} />{" "}
+          </Link>
+          <Link href="https://instagram.com/marksikaundi">
+            <FaInstagramSquare size={25} />{" "}
+          </Link>
+          <Link href="https://twitter.com/Alisikaundi">
+            <FaTwitterSquare size={25} />{" "}
+          </Link>
+        </div>
+      </div>
+      <div className="lg:col-span-2 mt-6 flex justify-between">
+        <div>
+          <h6 className="font-medium text-black bold">Products</h6>
+          <ul>
+            <li className="py-2 text-sm">
+              <Link href="/features">Features</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/courses">Courses</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/funding-programs">Fundings</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/lms">Learning System</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/docs">Docs</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/guides">Guides</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-black bold">Explore</h6>
+          <ul>
+            <li className="py-2 text-sm">
+              <Link href="/analytics">Analytics</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/commerce">Commerce</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/ai-tools">AI Tools</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/research">Research</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/help">Help</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/fqa">FQA</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 className="font-medium text-black bold">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="https://devcircleafrica.com/careers">careers</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/open-source">Open source</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/privacy">Privacy Policy</Link>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="/terms">Terms</Link>
+              <li className="py-2 text-sm">
+                <Link href="/code-hacks">Code Hacks</Link>
+              </li>
+              <li className="py-2 text-sm">
+                <Link href="/marketplace">Marketplace</Link>
+              </li>
+            </li>
+            <li className="py-2 text-sm">
+              <Link href="https://forum.devcircleafrica.com">Community</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex">
+        <span>&copy; {new Date().getFullYear()} Otofast Project</span>
+      </div>
     </div>
-  )
+  );
 }

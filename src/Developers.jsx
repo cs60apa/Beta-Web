825fbcb9df94c84421cacd752dfbdab86802f67d
AngelahMbula <mbula.angelah@gmail.com>
2023-09-21 21:48:15 +0300

    added content to solutions, developers and products page

--- src/Developers.jsx
+++ src/Developers.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 
 function Developers() {
-    return(
-        <div>here is developers</div>
-    );
+  return (
+    <div className="min-h-screen bg-[#040404] text-white">
+      <div className="bg-[url('/assets/milad.jpg')] h-screen">
+        <div className="flex justify-center text-center pt-40">
+          <p className="text-4xl md:text-6xl">
+            The <i>all-in-one</i> Platform<br></br> for your Independent Work.
+          </p>
+        </div>
+        <div className="flex justify-center mt-20">
+          <a
+            href="#"
+            className="flex justify-center border-none bg-white hover:bg-gray-300 text-black rounded-full w-40"
+          >
+            <p className="flex md:m-2 m-1 text-lg md:pb-1">Get Started</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Developers;
--- src/Solutions.jsx
+++ src/Solutions.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 function Solutions() {
-    return(
-        <div>
-            <p>solutions</p>
+  return (
+    <div className="min-h-screen bg-[#040404] text-white lg:p-10 p-3">
+      <div className="flex md:flex-row flex-col justify-between">
+        <p className="md:text-5xl text-3xl font-medium">
+          Working with DevCircle
+        </p>
+        <p className="flex text-lg font-bold underline underline-offset-4 decoration-[#00D4FF] hover:scale-110 mt-4">
+          Explore Devcircle <AiOutlineArrowRight className="mt-1.5 ml-1" />
+        </p>
+      </div>
+      <div className="flex md:flex-row flex-col mt-14 justify-between space-y-20 md:space-y-0">
+        <div className="border-none h-80 lg:w-96 md:w-80 space-y-3">
+          <img src="/assets/discover.webp" alt="" />
+          <h1 className="text-2xl font-bold">Professional Services</h1>
+          <p className="text-base">
+            Ready to start? Let's walk through Devcircle can help you launch
+            your tech career.
+          </p>
+          <p className="flex text-base font-bold underline underline-offset-4 decoration-[#00D4FF] hover:translate-x-6 hover:scale-110">
+            Learn More <AiOutlineArrowRight className="mt-1.5 ml-1" />
+          </p>
         </div>
-    );
+        <div className="border-none h-80 lg:w-96 md:w-80 space-y-2">
+          <img src="/assets/discover.webp" alt="" />
+          <h1 className="text-2xl font-bold">Consulting Partners</h1>
+          <p className="text-base">
+            Engage a certified partner to help with strategy, implementation,
+            manage services for your solution needs.
+          </p>
+          <p className="flex text-base font-bold underline underline-offset-4 decoration-[#00D4FF] hover:translate-x-6 hover:scale-110">
+            Learn More <AiOutlineArrowRight className="mt-1.5 ml-1" />
+          </p>
+        </div>
+        <div className="border-none h-80 lg:w-96 md:w-80 space-y-3">
+          <img src="/assets/discover.webp" alt="" />
+          <h1 className="text-2xl font-bold">Guide to Discover</h1>
+          <p className="text-base">
+            Ready to start? Let's walk through Devcircle can help you launch
+            your tech career.
+          </p>
+          <p className="flex text-base font-bold underline underline-offset-4 decoration-[#00D4FF] hover:translate-x-6 hover:scale-110">
+            Learn More <AiOutlineArrowRight className="mt-1.5 ml-1" />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Solutions;
